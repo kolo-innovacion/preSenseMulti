0b1fe9e762aa7615de3953edea4fa4708e0488e9
alejandrothackerkolo <44816682+alejandrothackerkolo@users.noreply.github.com>
2019-04-01 01:00:20 -0600

    initial

---
+++ preSenseMulti/preSenseMulti.ino
@@ -0,0 +1,42 @@
+class Sensor {
+    int powPin;
+    int pulPin;
+    int value;
+    int thresh;
+
+  public:
+    Sensor(int po, int pu) {
+      powPin = po;
+      pulPin = pu;
+      value = 0;
+      thresh = 0;
+
+      pinMode(pulPin, INPUT);
+      pinMode(powPin, OUTPUT);
+    }
+
+    void readValue() {
+      digitalWrite(powPin, HIGH);
+
+      int pulse = pulseIn(pulPin, HIGH);
+
+      value = (pulse * 2.54) / 147;
+      Serial.println(value);
+
+      digitalWrite(powPin, LOW);
+      //delay(500);
+    }
+
+};
+
+Sensor sensorA(2, 3);
+Sensor sensorB(4, 5);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorA.readValue();
+  sensorB.readValue();
+}
