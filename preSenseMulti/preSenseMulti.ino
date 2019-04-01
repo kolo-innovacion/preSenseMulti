417a9ba45c8d5bd12705716eb7333297043e4081
alejandrothackerkolo <44816682+alejandrothackerkolo@users.noreply.github.com>
2019-04-01 01:29:20 -0600

    BS Port added

--- preSenseMulti/preSenseMulti.ino
+++ preSenseMulti/preSenseMulti.ino
@@ -1,3 +1,7 @@
+//INICIA CONFIGURACION
+
+//TERMINA CONFIGURACION
+
 class Sensor {
     int powPin;
     int pulPin;
@@ -28,9 +32,57 @@ class Sensor {
 
 };
 
+//Brightsign gpio class definition:
+
+bool active = true;
+bool normal = false;
+
+int gDelay = 500;
+
+class Gport
+{
+    int pin;
+    int ledPin;
+
+  public:
+
+    void setNormal() {
+      digitalWrite(pin, normal);
+      digitalWrite(ledPin, LOW);
+    }
+
+    Gport(int gp, int lp) {
+      pin = gp;
+      ledPin = lp;
+
+      pinMode(pin, OUTPUT);
+      pinMode(ledPin, OUTPUT);
+      setNormal();
+    }
+    void setActive() {
+      digitalWrite(pin, active);
+      digitalWrite(ledPin, HIGH);
+      delay(gDelay);
+    }
+    void setOutput(bool input) {
+      if (input) {
+        //in case Ball has been punched, Brightsign active value is set
+        setActive();
+        setNormal();
+      } else {
+        setNormal();
+      }
+    }
+
+};
+
+//sensor objects creation
 Sensor sensorA(2, 3);
 Sensor sensorB(4, 5);
 Sensor sensorC(6, 7);
+
+//BS GPIO port creation
+Gport gport0(12, 13);
 
 void setup() {
   Serial.begin(9600);
